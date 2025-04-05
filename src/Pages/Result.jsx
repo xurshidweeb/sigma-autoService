dc4e84fb8027a7cba26538be02dee5e56a455057
ParpiboyevXurshidbek02 <162950670+ParpiboyevXurshidbek02@users.noreply.github.com>
2025-04-05 21:16:13 +0500

    result aos

--- src/Pages/Result.jsx
+++ src/Pages/Result.jsx
@@ -23,28 +23,28 @@ function Result() {
         <div className="bg-black/60 absolute w-full h-full top-0"></div>
         <div className="container ">
           <ul className="grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-            <li className="flex flex-col gap-2 items-center">
+            <li data-aos="flip-up" className="flex flex-col gap-2 items-center">
               <FaCheck className=" z-2 text-4xl text-white" />
               <div className=" z-2 text-white  text-4xl font-bold">15</div>
               <div className=" z-2 text-white font-medium text-md">
                 {t("result.text1")}
               </div>
             </li>
-            <li className="flex flex-col gap-2 items-center">
+            <li data-aos="flip-up" className="flex flex-col gap-2 items-center">
               <FaUsersCog className=" z-2 text-4xl text-white" />
               <div className=" z-2 text-white  text-4xl font-bold">20</div>
               <div className=" z-2 text-white font-medium text-md">
                 {t("result.text2")}
               </div>
             </li>
-            <li className="flex flex-col gap-2 items-center">
+            <li data-aos="flip-up" className="flex flex-col gap-2 items-center">
               <FaUsers className=" z-2 text-4xl text-white" />
               <div className=" z-2 text-white  text-4xl font-bold">1000</div>
               <div className=" z-2 text-white font-medium text-md">
                 {t("result.text3")}
               </div>
             </li>
-            <li className="flex flex-col gap-2 items-center">
+            <li data-aos="flip-up" className="flex flex-col gap-2 items-center">
               <FaCar className=" z-2 text-4xl text-white" />
               <div className=" z-2 text-white  text-4xl font-bold">500</div>
               <div className=" z-2 text-white font-medium text-md">
